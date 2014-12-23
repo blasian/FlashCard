6655ecb28bd3c45608b770ceba04c1b93dc829b5
blasian <mikespearman.e@gmail.com>
2014-12-22 22:05:15 -0800

    incorrect-correct added - ordering must be fixed.

---
+++ FlashCard/FLASHSection.h
@@ -0,0 +1,13 @@
+//
+//  FLASHSectionInterface.h
+//  FlashCard
+//
+//  Created by Michael Spearman on 12/22/14.
+//  Copyright (c) 2014 Michael Spearman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FLASHSectionInterface : NSObject
+
+@end
---
+++ FlashCard/FLASHSection.m
@@ -0,0 +1,13 @@
+//
+//  FLASHSectionInterface.m
+//  FlashCard
+//
+//  Created by Michael Spearman on 12/22/14.
+//  Copyright (c) 2014 Michael Spearman. All rights reserved.
+//
+
+#import "FLASHSectionInterface.h"
+
+@implementation FLASHSectionInterface
+
+@end
